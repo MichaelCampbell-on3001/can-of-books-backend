e3f535837f3f4e7c224f386fdd369dc46326d6ad
Daniel Brian <daniel.brian@gmail.com>
2022-03-01 23:40:03 -0500

    added delete route

--- models/book.js
+++ models/book.js
@@ -1,12 +1,13 @@
+// const { type } = require('express/lib/response');
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
 const bookSchema = new Schema({
-  title: String,
-  description: String,
-  status: String,
-  email: String
+  title: {type: String},
+  description: {type: String},
+  status: {type: String},
+  email: {type: String}
 });
 
 const Book = mongoose.model('Book', bookSchema);
